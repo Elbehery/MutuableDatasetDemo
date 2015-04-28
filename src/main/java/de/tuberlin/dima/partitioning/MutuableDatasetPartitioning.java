c736a22dbe1befb78d91e46f4b8a29f4af7641f2
elbehery <eng.m.behery@live.com>
2015-04-28 21:10:09 +0200

    Accumulator-Failed

--- src/main/java/de/tuberlin/dima/partitioning/MutuableDatasetPartitioning.java
+++ src/main/java/de/tuberlin/dima/partitioning/MutuableDatasetPartitioning.java
@@ -1,46 +1,112 @@
 package de.tuberlin.dima.partitioning;
 
 import de.tuberlin.dima.Person;
+import org.apache.flink.api.common.JobExecutionResult;
+import org.apache.flink.api.common.accumulators.Accumulator;
 import org.apache.flink.api.common.functions.MapFunction;
+import org.apache.flink.api.common.functions.RichMapFunction;
 import org.apache.flink.api.java.DataSet;
 import org.apache.flink.api.java.ExecutionEnvironment;
+import org.apache.flink.api.java.io.TypeSerializerOutputFormat;
+import org.apache.flink.api.java.tuple.Tuple2;
+import org.apache.flink.configuration.Configuration;
 import org.apache.flink.core.fs.FileSystem;
-import org.apache.flink.core.fs.Path;
-
-
-import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.HashMap;
 
 
 public class MutuableDatasetPartitioning {
 
+    private static final String TASK_INFO_ACCUMULATOR = "placementMap";
+
+
     public static void main(String[] args) {
 
         ExecutionEnvironment env = ExecutionEnvironment.getExecutionEnvironment();
-        env.setParallelism(3);
 
-        DataSet<String> personDataString = env.readTextFile("../MutuableDatasetDemo/src/main/resources/Person.csv");
-
-        DataSet<Person> personDataSet = personDataString.map(new PersonMapper());
-
-
-        // writing the dataset after partitioning with a custom outputFormat, trying to edit the output folder dynamically per each node
-        personDataSet.partitionByHash("name").write(new MutableDataSetTextOutputFormat<Person>(new Path()),"/home/mustafa/Documents/tst/", FileSystem.WriteMode.OVERWRITE);
+        DataSet<Person> in = env
+            .readTextFile("../MutuableDatasetDemo/src/main/resources/Person.csv")
+            .map(new PersonMapper());
 
         try {
+            // convert operator starts here
+            env.setParallelism(3);
+            in.partitionByHash("name") // TODO: as KeySelector
+                    .map(new TrackHostPerson())
+                    .write(new TypeSerializerOutputFormat(), "/home/mustafa/Documents/tst/", FileSystem.WriteMode.OVERWRITE);
 
-            // printing the execution plan to check the id of the node after re-partitioning
-            System.out.println(personDataSet.partitionByHash("name").getExecutionEnvironment().getExecutionPlan());
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+            //env.getExecutionPlan();
+           final JobExecutionResult result = env.execute();
+            HashMap<Integer, String> placementMap = result.getAccumulatorResult(TASK_INFO_ACCUMULATOR);
+            System.out.format("number of objects in the map =  %s\n", placementMap);
 
-        try {
-            env.execute();
+            // placementMap[0] = "host-where-slot-1-was-scheduled"
+            // placementMap[1] = "host-where-slot-2-was-scheduled"
+            // placementMap[2] = "host-where-slot-3-was-scheduled"
+            // convert operator ends here
+
         }catch (Exception e) {
             System.out.println("Error: " + e.getMessage());
         }
-
     }
+
+
+    public static class TrackHostPerson<T> extends RichMapFunction<T,T> {
+
+        private final TaskAssignmentAccumulator taskAssignmentAccumulator = new TaskAssignmentAccumulator();;
+
+        @Override
+        public void open(Configuration parameters) throws Exception {
+            super.open(parameters);
+            // adding the host info to the local accumulator
+            Tuple2<Integer,String> hostInfo = new Tuple2<Integer,String>(getRuntimeContext().getIndexOfThisSubtask(),getRuntimeContext().getTaskName());
+            this.taskAssignmentAccumulator.add(hostInfo);
+            getRuntimeContext().addAccumulator(TASK_INFO_ACCUMULATOR, this.taskAssignmentAccumulator);
+        }
+
+        @Override
+        public T map(T t) throws Exception {
+            return t;
+        }
+    }
+
+    public static  class TaskAssignmentAccumulator implements Accumulator<Tuple2<Integer, String>, ArrayList<Tuple2<Integer, String>>> {
+
+        private ArrayList<Tuple2<Integer, String>> hostInfo;
+
+        public TaskAssignmentAccumulator(){
+            hostInfo = new ArrayList<Tuple2<Integer, String>>();
+        }
+
+        @Override
+        public void add(Tuple2<Integer, String> value) {
+            this.hostInfo.add(value);
+        }
+
+        @Override
+        public ArrayList<Tuple2<Integer, String>> getLocalValue() {
+            return this.hostInfo;
+        }
+
+        @Override
+        public void resetLocal() {
+            this.hostInfo.clear();
+        }
+
+        @Override
+        public void merge(Accumulator<Tuple2<Integer, String>, ArrayList<Tuple2<Integer, String>>> other) {
+                this.hostInfo.addAll(other.getLocalValue());
+        }
+
+        @Override
+        public Accumulator<Tuple2<Integer, String>, ArrayList<Tuple2<Integer, String>>> clone() {
+
+            TaskAssignmentAccumulator clonedTaskAssignmentAccumulator = new TaskAssignmentAccumulator();
+            clonedTaskAssignmentAccumulator.hostInfo = this.hostInfo;
+            return clonedTaskAssignmentAccumulator;
+        }
+    }
+
 
     public static class PersonMapper implements MapFunction<String, Person> {
 
@@ -60,4 +126,5 @@ public class MutuableDatasetPartitioning {
             return person;
         }
     }
+
 }
